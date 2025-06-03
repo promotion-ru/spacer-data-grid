65ca7cb3c79ba4d7b1dea621eefc533f5a2c50b0
StarkOne <stark.pro40@gmail.com>
2025-06-03 10:38:20 +0700

    feat: Add Laravel IDE Helper and update configuration files

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+//use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+//// Публичные маршруты
+//Route::post('/register', [AuthController::class, 'register']);
+//Route::post('/login', [AuthController::class, 'login']);
+//
+//// Защищенные маршруты
+//Route::middleware('auth:sanctum')->group(function () {
+//    Route::get('/user', [AuthController::class, 'user']);
+//    Route::post('/logout', [AuthController::class, 'logout']);
+//});
+
+// Тестовый маршрут
+Route::get('/test', function () {
+    return response()->json(['message' => 'API is working!']);
 });
